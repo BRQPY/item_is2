aae6ff3d5ecebcbfb6f02c1acee98aa89f98a2cc
BRQPY <pcdruizdiaz@fpuna.edu.py>
2020-08-10 16:47:30 -0400

    Se agrega el env al gitignore y se cambia el nombre de prueba3 a item_is2

--- item_is2/wsgi.py
+++ item_is2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for prueba3 project.
+WSGI config for item_is2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prueba3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'item_is2.settings')
 
 application = get_wsgi_application()
