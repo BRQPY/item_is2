b232b066ac136b48abbb0ab33cf75ef994a34fdd
BRQPY <pcdruizdiaz@fpuna.edu.py>
2020-09-22 04:18:52 -0400

    Mejora en front de vista de proyecto y fase en proyecto iniciado

--- item_is2/settings.py
+++ item_is2/settings.py
@@ -162,19 +162,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
-'''STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    )'''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/item/item_is2/static',
+    '/var/www/item/item_is2/static/',
 ]
 #MEDIA_ROOT = ''
 MEDIA_URL = "/media/"
-
-#STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"),'/static/',]
 
 
 AWS_ACCESS_KEY_ID = 'AKIAUWIIW4ARQQQA4TLN'
--- item_is2/urls.py
+++ item_is2/urls.py
@@ -48,10 +48,6 @@ def permissionErrorView(request):
     return render(request, 'permissionError.html')
 
 
-urlpatterns = []
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
